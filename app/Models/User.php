75e0963d31417f0a8c6650662beaffa5dfc7faa4
Matias DEVEZE <matiasdeveze@gmail.com>
2024-09-20 13:53:29 +0200

    Modélisation des utilisateurs

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,5 +45,11 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    protected function username() : Attribute {
+        return Attribute::make(
+            get: fn() => "user" . str_pad((string) $this->id, 4 , "0", STR_PAD_LEFT),
+        );
     }
 }
