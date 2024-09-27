f2de8ffe34bf1b218cd54f3c56b04f5a076bcd1a
Matias DEVEZE <matiasdeveze@gmail.com>
2024-09-27 16:27:22 +0200

    Modélisation des codes d’inscription

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasMany;
 
     /**
      * The attributes that are mass assignable.
@@ -50,5 +51,10 @@ class User extends Authenticatable
         return Attribute::make(
             get: fn() => "user" . str_pad((string) $this->id, 4 , "0", STR_PAD_LEFT),
         );
+    }
+
+    public function invitationCodes(): HasMany
+    {
+        return $this->hasMany(InvitationCode::class);
     }
 }
